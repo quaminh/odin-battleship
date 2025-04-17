f00da09c804419c4cb9667267d4b65d7f878d6ba
Quamin Hua <quaminhua56@gmail.com>
2025-04-17 13:58:10 -0700

    Add functionality to gameboard class

--- src/gameboard.js
+++ src/gameboard.js
@@ -19,11 +19,47 @@ export default class Gameboard {
     }
   }
 
-  placeShip(x, y, size, isHorizontal = true) {}
+  placeShip(x, y, size, isHorizontal = true) {
+    if (x < 0 || x >= this.#boardLength || y < 0 || y >= this.#boardLength)
+      return false;
+    if (isHorizontal) {
+      if (y + size - 1 >= this.#boardLength) return false;
+      for (let i = 0; i < size; i++) {
+        if (this.board[x][y + i].ship) return false;
+      }
+      const ship = new Ship(size);
+      this.ships.push(ship);
+      for (let i = 0; i < size; i++) {
+        this.board[x][y + i].ship = ship;
+      }
+    } else {
+      if (x + size - 1 >= this.#boardLength) return false;
+      for (let i = 0; i < size; i++) {
+        if (this.board[x + i][y].ship) return false;
+      }
+      const ship = new Ship(size);
+      this.ships.push(ship);
+      for (let i = 0; i < size; i++) {
+        this.board[x + i][y].ship = ship;
+      }
+    }
+    return true;
+  }
 
-  receiveAttack(x, y) {}
+  receiveAttack(x, y) {
+    const tile = this.board[x][y];
+    if (tile.attacked) return false;
+    tile.attacked = true;
+    if (tile.ship) tile.ship.hit();
+    return true;
+  }
 
-  allShipsSunk() {}
+  allShipsSunk() {
+    for (let i = 0; i < this.ships.length; i++) {
+      if (!this.ships[i].isSunk()) return false;
+    }
+    return true;
+  }
 }
 
 class Tile {
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -7,74 +7,83 @@ test('create gameboard', () => {
   expect(gameboard.board[0][0].ship).toBeNull();
 });
 
-// test('place ship horizontal', () => {
-//   const gameboard = new Gameboard();
-//   gameboard.placeShip(0, 0, 3, true);
-//   expect(gameboard.board[0][0].ship).not.toBeNull();
-//   expect(gameboard.board[0][1].ship).not.toBeNull();
-//   expect(gameboard.board[0][2].ship).not.toBeNull();
-// });
+test('place ship horizontal', () => {
+  const gameboard = new Gameboard();
+  gameboard.placeShip(0, 0, 3, true);
+  expect(gameboard.board[0][0].ship).not.toBeNull();
+  expect(gameboard.board[0][1].ship).not.toBeNull();
+  expect(gameboard.board[0][2].ship).not.toBeNull();
+});
 
-// test('place ship vertical', () => {
-//   const gameboard = new Gameboard();
-//   gameboard.placeShip(0, 0, 3, false);
-//   expect(gameboard.board[0][0].ship).not.toBeNull();
-//   expect(gameboard.board[1][0].ship).not.toBeNull();
-//   expect(gameboard.board[2][0].ship).not.toBeNull();
-// });
+test('place ship vertical', () => {
+  const gameboard = new Gameboard();
+  gameboard.placeShip(0, 0, 3, false);
+  expect(gameboard.board[0][0].ship).not.toBeNull();
+  expect(gameboard.board[1][0].ship).not.toBeNull();
+  expect(gameboard.board[2][0].ship).not.toBeNull();
+});
 
-// test('prevent placing ship on top of another ship', () => {
-//   const gameboard = new Gameboard();
-//   gameboard.placeShip(5, 5, 3, true);
-//   expect(gameboard.placeShip(5, 5, 2, true)).toBe(false);
-//   expect(gameboard.placeShip(5, 7, 2, true)).toBe(false);
-//   expect(gameboard.placeShip(4, 5, 2, true)).toBe(false);
-// });
+test('prevent placing ship on top of another ship', () => {
+  const gameboard = new Gameboard();
+  gameboard.placeShip(5, 5, 3, true);
+  expect(gameboard.placeShip(5, 5, 2, true)).toBe(false);
+  expect(gameboard.placeShip(5, 7, 2, true)).toBe(false);
+  expect(gameboard.placeShip(5, 4, 2, true)).toBe(false);
+});
 
-// test('prevent placing ship out of bounds', () => {
-//   const gameboard = new Gameboard();
-//   expect(gameboard.placeShip(10, 0, 2, true)).toBe(false);
-//   expect(gameboard.placeShip(0, 10, 2, true)).toBe(false);
-// });
+test('prevent placing ship out of bounds', () => {
+  const gameboard = new Gameboard();
+  expect(gameboard.placeShip(10, 0, 2, true)).toBe(false);
+  expect(gameboard.placeShip(0, 10, 2, true)).toBe(false);
+});
 
-// test('prevent placing ship that would extend out of bounds', () => {
-//   const gameboard = new Gameboard();
-//   expect(gameboard.placeShip(9, 9, 2, true)).toBe(false);
-//   expect(gameboard.placeShip(9, 9, 2, false)).toBe(false);
-//   expect(gameboard.board[9][9].ship).toBeNull();
-// });
+test('prevent placing ship that would extend out of bounds', () => {
+  const gameboard = new Gameboard();
+  expect(gameboard.placeShip(9, 9, 2, true)).toBe(false);
+  expect(gameboard.placeShip(9, 9, 2, false)).toBe(false);
+  expect(gameboard.board[9][9].ship).toBeNull();
+});
 
-// test('receive attack and hit', () => {
-//   const gameboard = new Gameboard();
-//   gameboard.placeShip(0, 0, 2, false);
-//   expect(gameboard.receiveAttack(0, 0)).toBe(true);
-//   expect(gameboard.receiveAttack(1, 0)).toBe(true);
-//   expect(gameboard.board[0][0].ship.hitCount).toBe(2);
-// });
+test('receive attack and hit', () => {
+  const gameboard = new Gameboard();
+  gameboard.placeShip(0, 0, 2, false);
+  expect(gameboard.receiveAttack(0, 0)).toBe(true);
+  expect(gameboard.receiveAttack(1, 0)).toBe(true);
+  expect(gameboard.board[0][0].ship.hitCount).toBe(2);
+});
 
-// test('receive attack and miss', () => {
-//   const gameboard = new Gameboard();
-//   expect(gameboard.receiveAttack(0, 0)).toBe(false);
-// });
+test('receive attack and miss', () => {
+  const gameboard = new Gameboard();
+  expect(gameboard.receiveAttack(0, 0)).toBe(true);
+});
 
-// test('check all ships sunk with one ship', () => {
-//   const gameboard = new Gameboard();
-//   gameboard.placeShip(0, 0, 2, true);
-//   gameboard.receiveAttack(0, 0);
-//   gameboard.receiveAttack(0, 1);
-//   expect(gameboard.allShipsSunk()).toBe(true);
-// });
+test('prevent attacking in same spot', () => {
+  const gameboard = new Gameboard();
+  gameboard.receiveAttack(0, 0);
+  expect(gameboard.receiveAttack(0, 0)).toBe(false);
+});
 
-// test ('check all ships sunk with multiple ships', () => {
-//   const gameboard = new Gameboard();
-//   gameboard.placeShip(0, 0, 2, true);
-//   gameboard.placeShip(1, 0, 2, true);
-//   gameboard.placeShip(2, 0, 2, false);
-//   gameboard.receiveAttack(0, 0);
-//   gameboard.receiveAttack(0, 1);
-//   gameboard.receiveAttack(1, 0);
-//   gameboard.receiveAttack(1, 1);
-//   gameboard.receiveAttack(2, 0);
-//   gameboard.receiveAttack(3, 0);
-//   expect(gameboard.allShipsSunk()).toBe(true);
-// });
+test('check all ships sunk with one ship', () => {
+  const gameboard = new Gameboard();
+  gameboard.placeShip(0, 0, 2, true);
+  gameboard.receiveAttack(0, 0);
+  expect(gameboard.allShipsSunk()).toBe(false);
+  gameboard.receiveAttack(0, 1);
+  expect(gameboard.allShipsSunk()).toBe(true);
+});
+
+test ('check all ships sunk with multiple ships', () => {
+  const gameboard = new Gameboard();
+  gameboard.placeShip(0, 0, 2, true);
+  gameboard.placeShip(1, 0, 2, true);
+  gameboard.placeShip(2, 0, 2, false);
+  gameboard.receiveAttack(0, 0);
+  gameboard.receiveAttack(0, 1);
+  expect(gameboard.allShipsSunk()).toBe(false);
+  gameboard.receiveAttack(1, 0);
+  gameboard.receiveAttack(1, 1);
+  expect(gameboard.allShipsSunk()).toBe(false);
+  gameboard.receiveAttack(2, 0);
+  gameboard.receiveAttack(3, 0);
+  expect(gameboard.allShipsSunk()).toBe(true);
+});
